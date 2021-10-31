201039b8afa6411beb843518c3d9d1215e982d64
ouda <616559837@qq.com>
2021-10-31 14:46:12 +0800

    插入排序重构

--- 算法/插入排序.js
+++ 算法/插入排序.js
@@ -5,13 +5,15 @@
  * 从而一个新的、记录数增1的有序表。在其实现过程使用双层循环，外层循环对除了第一个元素之外的所有元素，
  * 内层循环对当前元素前面有序表进行待插入位置查找，并进行移动
  */
-let s = [2, 1, 3, 10, 4, 5, 8, 11, 9, -2, 100, 99, -30, -4]
+// 插入排序
 const insertSort = function (arr) {
+  // 遍历数组 从下标为1的位置开始遍历 (下标为0的数据默认已经参与了排序)
   for (let i = 1; i < arr.length; i++) {
-    // 当前需要插入的数据
+    //		下标为 i 的数据为待插入数据 ; 下一个问题:找到待插入位置
     let currentNumber = arr[i]
-    let insertIndex = i - 1
-    while (insertIndex >= 0 && currentNumber < arr[insertIndex]) {
+    let insertIndex = i - 1// 待插入位置，默认当前为插入位置，index一直左移，直到找到位置
+    // 合适的位置：  插入的数据比移动下标对应的数据要大
+    while (insertIndex >= 0 && arr[insertIndex] > currentNumber) {
       arr[insertIndex + 1] = arr[insertIndex]
       insertIndex--
     }
@@ -19,4 +21,6 @@ const insertSort = function (arr) {
   }
   return arr
 }
+
+const s = [22, 8, 16, 5, 47, 1, 10, 3, 15]
 console.log(insertSort(s))

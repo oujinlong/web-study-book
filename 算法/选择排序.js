eb9665b7f1d174fce2644b4b2ba6a9fc7196ece4
ouda <616559837@qq.com>
2021-10-31 14:02:42 +0800

    选择排序重构

--- 算法/选择排序.js
+++ 算法/选择排序.js
@@ -4,22 +4,25 @@
  * 存放在序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，
  * 然后放到已排序序列的末尾。以此类推，直到全部待排序的数据元素排完。
  */
-
-let s = [2, 1, 3, 10, 4, 5, 8, 11, 9, -2, 100, 99, -30]
-// 选择排序
+//选择排序
 const chooseSort = function (arr) {
+  // 遍历数组找出最小元素，放在排序末尾
   for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[minIndex] > arr[j]) {
-        minIndex = j
+    let minNumberIndex = i; // 记录最小数据的下标， 默认为 循环的 i，即未参加排序的第一个数据
+    for (let j = i + 1; j < arr.length; j++) { // 遍历未排序部分的数据，目的找到最小值的 index
+      if (arr[minNumberIndex] > arr[j]) {
+        // 最小值下标替换
+        minNumberIndex = j
       }
     }
-    // 交换位置
-    let temp = arr[minIndex]
-    arr[minIndex] = arr[i]
-    arr[i] = temp
+    // 最小值下标对应的为未参加排序的数据中最小值的下标，将数据和 i 下标数据交换，第 i 个即算入已参加排序的数据
+    let temp = arr[i]
+    arr[i] = arr[minNumberIndex]
+    arr[minNumberIndex] = temp
   }
   return arr
 }
-console.log({ res: chooseSort(s) })
+
+
+const s = [22, 8, 16, 5, 47, 1, 10, 3, 15]
+console.log(chooseSort(s))
